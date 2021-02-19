9ddfeef6d3f1357c57b95db7c458a38a0bea0d48
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2021-02-19 14:16:35 -0700

    Rename chunk math

--- src/main/kotlin/Common.kt
+++ src/main/kotlin/Common.kt
@@ -16,18 +16,20 @@ public inline fun ceil(x: Double, base: Double): Double = ceil(x / base) * base
 public inline fun roundToPrecision(x: Double, precision: Int): Double = round(x, 1.0 / 10.0.pow(precision))
 public inline fun nearest(x: Double, a: Double, b: Double): Double = if (abs(x - a) < abs(x - b)) a else b
 
-public inline infix fun Double.prem(x: Double): Double = (rem(x) + x) % x
+public inline infix fun Double.remChunk(x: Double): Double = (rem(x) + x) % x
+public inline infix fun Double.divChunk(x: Double): Double = minus(remChunk(x)) / x
 public inline fun lerp(a: Double, b: Double, t: Double): Double = a + (b - a) * t
 public inline fun unlerp(a: Double, b: Double, x: Double): Double = if (a == b) 0.0 else (x - a) / (b - a)
 public inline fun remap(a1: Double, b1: Double, a2: Double, b2: Double, x: Double): Double =
     lerp(a2, b2, unlerp(a1, b1, x))
 
-public inline infix fun Int.prem(x: Int): Int = (rem(x) + x) % x
+public inline infix fun Int.remChunk(x: Int): Int = (rem(x) + x) % x
+public inline infix fun Int.divChunk(x: Int): Int = minus(remChunk(x)) / x
 public inline fun lerp(a: Int, b: Int, t: Double): Int = lerp(a.toDouble(), b.toDouble(), t).toInt()
 public inline fun <T> cerp(a: T, b: T, t: Double): T = if (t < 1.0) a else b
 
 public inline fun repeated(a: Double, b: Double, x: Double): Double = lerp(a, b, repeated(unlerp(a, b, x)))
-public inline fun repeated(x: Double): Double = x prem 1.0
+public inline fun repeated(x: Double): Double = x remChunk 1.0
 
 public inline fun reflected(a: Double, b: Double, x: Double): Double = lerp(a, b, reflected(unlerp(a, b, x)))
 public inline fun reflected(x: Double): Double {
