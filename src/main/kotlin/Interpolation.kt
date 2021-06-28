1d1b5bd3e2aaaf3884c2c47481b046b217adf13e
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2021-06-28 14:06:32 -0600

    Clean up some interpolation code

--- src/main/kotlin/Interpolation.kt
+++ src/main/kotlin/Interpolation.kt
@@ -5,41 +5,6 @@ package mathx
 
 import kotlin.math.abs
 
-// Interpolation
-
-public fun interface Interpolated<T> {
-    public fun interpolate(b: T, t: Double): T
-}
-
-public fun interface Interpolator<T> {
-    public fun interpolate(a: T, b: T, t: Double): T
-}
-
-public class InterpolatedInterpolator<T : Interpolated<T>> : Interpolator<T> {
-    override fun interpolate(a: T, b: T, t: Double): T = a.interpolate(b, t)
-}
-
-// Easing
-
-public fun interface Easing {
-    public fun ease(t: Double): Double
-}
-
-public inline fun <T> Interpolated<T>.interpolate(b: T, t: Double, easing: Easing): T =
-    interpolate(b, easing.ease(t))
-
-public inline fun <T> Interpolator<T>.interpolate(a: T, b: T, t: Double, easing: Easing): T =
-    interpolate(a, b, easing.ease(t))
-
-public inline fun Easing.easeOut(t: Double): Double = 1.0 - ease(1.0 - t)
-
-public fun Easing.easeInOut(t: Double): Double =
-    if (t < 0.5) ease(2.0 * t) / 2.0 else easeOut(2.0 * t - 1.0) / 2.0 + 0.5
-
-public inline operator fun Easing.unaryMinus(): Easing = Easing(::easeOut)
-
-// Helpers
-
 public inline fun <T> cerp(a: T, b: T, t: Double, threshold: Double): T = if (t < threshold) a else b
 public inline fun <T> cerp(a: T, b: T, t: Double): T = cerp(a, b, t, 1.0)
 public inline fun lerp(a: Double, b: Double, t: Double): Double = a + (b - a) * t
@@ -55,3 +20,60 @@ public inline fun reflected(x: Double): Double {
     val dist = abs(x) % 2.0
     return if (dist < 1.0) dist else 2.0 - dist
 }
+
+// Interfaces
+
+public fun interface Interpolated<T> {
+    public fun interpolate(b: T, t: Double): T
+}
+
+public fun interface Interpolator<T> {
+    public fun interpolate(a: T, b: T, t: Double): T
+}
+
+public fun interface Easing : Interpolated<Easing> {
+    public fun ease(t: Double): Double
+    override fun interpolate(b: Easing, t: Double): Easing = Easing { lerp(ease(it), b.ease(it), t) }
+}
+
+// Common
+
+public inline fun Easing.easeReversed(t: Double): Double = 1.0 - ease(1.0 - t)
+public inline fun Easing.easeWithReverse(t: Double, reverse: Easing = this): Double =
+    if (t < 0.5) ease(2.0 * t) / 2.0 else reverse.easeReversed(2.0 * t - 1.0) / 2.0 + 0.5
+
+public inline fun Easing.reversed(): Easing = Easing(::easeReversed)
+public inline fun Easing.withReverse(reverse: Easing = this): Easing = Easing { easeWithReverse(it, reverse) }
+
+public inline operator fun Easing.unaryMinus(): Easing = reversed()
+public inline operator fun Easing.minus(reverse: Easing): Easing = withReverse(reverse)
+
+public inline fun <T> Interpolated<T>.interpolateEased(b: T, t: Double, easing: Easing): T =
+    interpolate(b, easing.ease(t))
+
+public inline fun <T> Interpolator<T>.interpolateEased(a: T, b: T, t: Double, easing: Easing): T =
+    interpolate(a, b, easing.ease(t))
+
+public inline fun <T> Interpolated<T>.interpolateEasedReversed(b: T, t: Double, easing: Easing): T =
+    interpolate(b, easing.easeReversed(t))
+
+public inline fun <T> Interpolator<T>.interpolateEasedReversed(a: T, b: T, t: Double, easing: Easing): T =
+    interpolate(a, b, easing.easeReversed(t))
+
+public inline fun <T> Interpolated<T>.interpolateEasedWithReverse(
+    b: T,
+    t: Double,
+    easing: Easing,
+    reverse: Easing = easing,
+): T = interpolate(b, easing.easeWithReverse(t, reverse))
+
+public inline fun <T> Interpolator<T>.interpolateEasedWithReverse(
+    a: T,
+    b: T,
+    t: Double,
+    easing: Easing,
+    reverse: Easing = easing,
+): T = interpolate(a, b, easing.easeWithReverse(t, reverse))
+
+public inline infix fun <T> Interpolator<T>.withEasing(easing: Easing): Interpolator<T> =
+    Interpolator { a, b, t -> interpolateEased(a, b, t, easing) }
